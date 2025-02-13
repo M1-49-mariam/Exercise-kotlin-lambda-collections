dd58084cdf51f36b175783828db109c376797883
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-13 20:38:45 +0300

    incomplete - last task left

---
+++ src/Part 5.kt
@@ -0,0 +1,7 @@
+//fun processNumbers(numbers: List<Int>, filterFun: (Int) -> Boolean, squareFun: (Int) -> Boolean) : List<Int>{ }
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20)
+
+
+}
