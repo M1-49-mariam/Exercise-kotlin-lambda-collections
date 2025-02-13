738bb0a2a8516665335c8a920b93684296ed5d8f
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-13 20:31:51 +0300

    Part 4 - incomplete

--- src/Part 4.kt
+++ src/Part 4.kt
@@ -1,5 +1,12 @@
-//fun customerFilter(numbers: List<Int>, filter:(Int) -> Boolean): List<Int>
+fun customFilter(numbers: List<Int>, filterFun: (Int) -> Boolean): List<Int>{
+    return numbers.filter(filterFun)
+}
 
 fun main() {
+
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(customFilter(numbers) {it > 5})
+    println(customFilter(numbers) {it % 2 == 0})
+    println(customFilter(numbers) {it % 3 == 0})
 
 }
