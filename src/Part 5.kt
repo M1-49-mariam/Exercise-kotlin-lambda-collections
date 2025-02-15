f5a8b9de7ab2e7945877c0ed09872868c3d477d0
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-15 11:28:30 +0300

    done

--- src/Part 5.kt
+++ src/Part 5.kt
@@ -1,7 +1,12 @@
-//fun processNumbers(numbers: List<Int>, filterFun: (Int) -> Boolean, squareFun: (Int) -> Boolean) : List<Int>{ }
+fun processNumbers(numbers: List<Int>) : List<Int>{
+    val filterOddNumbers = { number1: Int -> number1 % 2 == 0}
+    val squareNumbers = { number2: Int -> number2 * number2 }
+    return numbers.filter(filterOddNumbers).map(squareNumbers)
+}
 
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20)
 
+    println(processNumbers(numbers))
 
 }
